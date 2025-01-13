bc62c8957694886475d6949988419c45a2384748
Ozmanov Alik <alik.ozmanov@fms-ea.com>
2025-01-13 13:25:33 +0100

    using thymeleaf to display them in the view

--- src/main/java/fr/fms/web/ContactController.java
+++ src/main/java/fr/fms/web/ContactController.java
@@ -1,9 +1,13 @@
 package fr.fms.web;
 
 import fr.fms.dao.ContactRepository;
+import fr.fms.entities.Contact;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class ContactController {
@@ -11,7 +15,11 @@ public class ContactController {
     ContactRepository contactRepository;
 
     @GetMapping("/index")
-    public String index() {
-        return "contacts";
-    }
+        public String index (Model model){
+            List<Contact> contacts = contactRepository.findAll();
+            model.addAttribute("listContact", contacts);
+
+            return "contacts";
+        }
 }
+
